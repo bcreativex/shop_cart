862af51412066e36c81dab054d839c5c40df13a8
krishna <krishnajatav504@gmail.com>
2024-05-27 22:53:50 +0530

    product list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar';
+import ProductList from './Component/ProductList';
 
 function App() {
+  const product = [
+    {
+      price: 99999,
+      name: "iPhone 15 Pro",
+      quantity: 0,
+    },
+    {
+      price: 99999,
+      name: "Realme 15 Pro",
+      quantity: 0,
+    }
+  ]
   return (
     <>
     <Navbar/>
+    <ProductList product={product}/>
+    <Footer/>
     </>
   );
 }
---
+++ src/Component/Product.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Product(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
