8714267718d30e9b187f3dcf245990db1bc047d7
krishna <krishnajatav504@gmail.com>
2024-05-28 15:51:35 +0530

    product quantity

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export default function Product(props) {
+
+    function incrementQuantity(index){
+
+    }
+
   return (
-    <div className='row'>
-        <div className='col-6'>
-            <h2>{props.product.name}<span class="badge text-bg-secondary">{props.product.price}</span></h2>
-        </div> 
+    <div className="row">
+      <div className="col-5">
+        <h2>
+          {props.product.name}
+          <span class="badge text-bg-secondary">₹{props.product.price}</span>
+        </h2>
+      </div>
+      <div className="col-3">
+        <div
+          class="btn-group"
+          role="group"
+          aria-label="Basic mixed styles example"
+        >
+          <button type="button" class="btn btn-danger">
+            -
+          </button>
+          <button type="button" class="btn btn-warning">
+            {props.product.quantity}
+          </button>
+          <button type="button" class="btn btn-success" onClick={this.incrementQuantity ()}>
+            + 
+          </button>
+        </div>
+      </div>
+      <div className="col-4">
+         {props.product.quantity*props.product.price}
+      </div>
     </div>
-  )
+  );
 }
